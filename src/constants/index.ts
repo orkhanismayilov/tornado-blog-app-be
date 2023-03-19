e2f3ea0a66d7aacb69a16d66642398833681aa5f
orkhanismayilov <orikismayilov@outlook.com>
2023-03-19 14:32:11 +0400

    Handle max image size for upload

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const mimeTypeMap = new Map([
+  ['image/png', 'png'],
+  ['image/jpeg', 'jpg'],
+  ['image/jpg', 'jpg'],
+  ['image/webp', 'webp'],
+  ['image/svg+xml', 'svg'],
+]);
+
+export const maxFileSize = 10;
